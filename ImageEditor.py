55db91db6b524d73d2bbc06e49e72a96aa8b52d2
Paul Rodriguez <paulyd0123@gmail.com>
2025-03-25 17:05:13 -0400

    added contrast feature to the script

--- ImageEditor.py
+++ ImageEditor.py
@@ -7,7 +7,12 @@ pathOut = '/editedImgs'
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
     
+    #Sharpens and desaturates image
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
+    
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
     
     clean_name =os.path.splitext(filename)[0]
         
